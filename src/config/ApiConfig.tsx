54a4c2e38f6ec89c69283d6229ca74420e3d2099
Dmitri Lisokonov <dmitri.lisokonov@gmail.com>
2021-12-20 14:30:23 +0100

    switching to https

--- src/config/ApiConfig.tsx
+++ src/config/ApiConfig.tsx
@@ -1,4 +1,4 @@
 export class ApiConfig {
-    public url: String = "http://172.16.1.2:5000/"
+    public url: String = "https://blueshop.tech:5000/"
     //public url: String = "https://localhost:44350/"
 }
